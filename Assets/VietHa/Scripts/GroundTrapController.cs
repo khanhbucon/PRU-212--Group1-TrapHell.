698d57502220e889c607a6121199dbc5f3e1246e
ISOBEAR\Admin <haviet030404@gmail.com>
2025-07-16 13:17:15 +0700

    1607

---
+++ Assets/VietHa/Scripts/GroundTrapController.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GroundTrapController : MonoBehaviour
+{
+    public enum GroundTrapType { Disappear, MoveLeft, MoveRight, MoveUp, None }
+    public GroundTrapType trapType = GroundTrapType.None;
+
+    public float trapSpeed = 5f;
+    private bool isActivated = false;
+    private Rigidbody2D rb;
+
+    [SerializeField] private Collider2D trapCollider; // ← gán collider nằm ở con
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        if (trapType == GroundTrapType.Disappear && trapCollider != null)
+        {
+            trapCollider.enabled = true;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (isActivated) return;
+
+        if (collision.CompareTag("Player"))
+        {
+            Debug.Log("Player đã vào trigger!");
+            isActivated = true;
+            ActivateGroundTrap();
+        }
+    }
+
+    void ActivateGroundTrap()
+    {
+        switch (trapType)
+        {
+            case GroundTrapType.Disappear:
+                if (trapCollider != null)
+                    //Destroy(trapCollider.gameObject);
+                    trapCollider.gameObject.GetComponent<TilemapRenderer>().enabled=false;
+                break;
+
+            case GroundTrapType.MoveLeft:
+                rb.bodyType = RigidbodyType2D.Kinematic;
+                rb.linearVelocity = Vector2.left * trapSpeed;
+                break;
+
+            case GroundTrapType.MoveRight:
+                rb.bodyType = RigidbodyType2D.Kinematic;
+                rb.linearVelocity = Vector2.right * trapSpeed;
+                break;
+
+            case GroundTrapType.MoveUp:
+                rb.bodyType = RigidbodyType2D.Kinematic;
+                rb.linearVelocity = Vector2.up * trapSpeed;
+                break;
+
+            case GroundTrapType.None:
+                break;
+        }
+    }
+}
---
+++ Assets/VietHa/Scripts/LevelManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LevelManager : MonoBehaviour
+{
+    
+    public static LevelManager Instance;// Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Awake()
+    {
+
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+
+    // Update is called once per frame
+    public void ReloadCurrentScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+
+}
